6f60a547ea1ca05e5d1d17c0135e08379d909dc5
Luchyihezie <luchyihez@gmail.com>
2022-12-11 03:11:17 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - single digits in base 16
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
+	
