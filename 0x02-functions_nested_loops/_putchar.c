9665d3863df065f7ef52ec83f800d5b375e1fdaa
Luchyihezie <luchyihez@gmail.com>
2022-12-13 13:30:11 -0800

    new task 1 commit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 /**
  * putchar - writes the character c to standard out put
- * @c: the character to print
+ * @c: The character to print
+ *
  * Return: on success 1
+ * On error, -1 is returned, and errno isset appropriately
  */
 
 int _putchar(char c)
