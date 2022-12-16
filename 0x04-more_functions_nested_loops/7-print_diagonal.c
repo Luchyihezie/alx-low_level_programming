2d7cb688b25bde33042b3fdac31f7462d90a7419
Luchyihezie <luchyihez@gmail.com>
2022-12-16 15:05:29 -0800

    task seven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  print_diagonal - draws adiagonal line on the terminal
- *  @n: number of times the character \ should be printed
- */
+* print_diagonal - draws adiagonal line on the terminal
+* @n: number of times the character \ should be printed
+*/
 
 void print_diagonal(int n)
 {
@@ -12,14 +12,15 @@ void print_diagonal(int n)
 	} else
 	{
 		int i, j;
-		
-		for (i = 0; i < n; i++)
+
+		for (i =0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (j == i)
+				if (j== i)
 					_putchar('\\');
-				else if (j < i);
+				else if (j < i)
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
